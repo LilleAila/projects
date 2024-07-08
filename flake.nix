87f02c69a1ae4469bc97c1279f5312ecbc0e38b8
LilleAila <olai.solsvik@gmail.com>
2024-07-08 10:56:26 +0200

    added missing stuff to js shell

--- flake.nix
+++ flake.nix
@@ -32,8 +32,10 @@
           };
           javascript = mkShell {
             nativeBuildInputs = [
-              typescript
+              nodejs
+              nodePackages.npm
               nodePackages.typescript-language-server
+              nodePackages.typescript
               prettierd
               emmet-ls
               vscode-langservers-extracted
