fe677d7e6655ec5fd7d277c20667475d5c34c4fe
LilleAila <olai.solsvik@gmail.com>
2024-09-06 12:10:46 +0200

    2fa

--- it2/03-bruker-innlogging/main.py
+++ it2/03-bruker-innlogging/main.py
@@ -1,6 +1,7 @@
 from getpass import getpass  # Innebygget i python
 import bcrypt
 from tinydb import TinyDB, Query
+import pyotp
 
 class UserDoesNotExistError(Exception):
     pass
@@ -8,6 +9,8 @@ class UserDoesNotExistError(Exception):
 class UserAlreadyExistsError(Exception):
     pass
 
+class DuplicateUserError(Exception):
+    pass
 
 class Login:
     def __init__(self):
@@ -18,6 +21,15 @@ class Login:
         _input = ""
         while True:
             _input = input(prompt).strip()  # Spørre bruker og fjerne ekstra mellomrom
+            if len(_input) > 0:
+                return _input
+            print("Du må skrive inn noe!")
+
+    # Samme som over men bruker getpass
+    def _input_password(self, prompt):
+        _input = ""
+        while True:
+            _input = getpass(prompt).strip()  # Spørre bruker og fjerne ekstra mellomrom
             if len(_input) > 0:
                 return _input
             print("Du må skrive inn noe!")
@@ -40,22 +52,46 @@ class Login:
             "type": "user",
             "username": username,
             # tinydb trenger en vanlig string, ikke bytes, mens bcrypt er motsatt.
-            "password": bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+            "password": bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8"),
+            "2fa": None
         })
+
+    def _get_2fa(self, username):
+        # Add or get the 2fa key for a user.
+        user = self._get_user(username)
+        if user["2fa"] is not None:
+            return user["2fa"]
+        else:
+            key = pyotp.random_base32()
+            self._db.update({
+                "2fa": key
+            }, Query().username == username)
+            return key
+
+    def _get_user(self, username):
+        users = self._db.search(Query().username == username)
+        if len(users) == 0:
+            raise UserDoesNotExistError(f"The user {username} does not exist!")
+        if len(users) > 1:
+            raise DuplicateUserError(f"There are multiple users with the username {username}!")
+        return users[0]
 
     def _list_users(self):
         users = self._db.all()
         print("Alle brukere:")
         print("\n".join([user["username"] for user in users if user["type"] == "user"]))
 
-    # Samme som over men bruker getpass
-    def _input_password(self, prompt):
-        _input = ""
+    def _verify_2fa(self, username):
+        # Antar at 2fa allerede er satt opp.
+        # Dette bør sjekkes før funksjonen calles, eller lages en egen error for det
+        input_code = ""
         while True:
-            _input = getpass(prompt).strip()  # Spørre bruker og fjerne ekstra mellomrom
-            if len(_input) > 0:
-                return _input
-            print("Du må skrive inn noe!")
+            input_code = input("Skriv inn koden fra autentiserings-appen din: ")
+            if len(input_code) == 6 and input_code.isdigit():
+                break
+            print("Koden er ugyldig!")
+        totp = pyotp.TOTP(self._get_2fa(username))
+        return totp.verify(input_code)
 
     def _login(self):
         print("Logger inn!")
@@ -65,6 +101,10 @@ class Login:
             password = self._input_password("Passordet ditt: ")
             try:
                 if self._check_password(username, password):
+                    if self._get_user(username)["2fa"] is not None:
+                        if not self._verify_2fa(username):
+                            print("Koden er feil!")
+                            continue
                     self._username = username
                     self._success()
                     break
@@ -101,6 +141,28 @@ class Login:
             else:
                 self._signup()
 
+    def _config_2fa(self):
+        while True:
+            print("""
+Du kan:
+- (s)ett opp 2fa
+- (t)est 2fa
+- (g)å tilbake
+                """)
+            action = self._input("Hva vil du gjøre? ")
+            print()
+            match action:
+                case "s":
+                    code = self._get_2fa(self._username)
+                    print(f"Skriv inn den følgende koden i autentiserings-appen din: {code}")
+                case "t":
+                    if self._verify_2fa(self._username):
+                        print("Koden er riktig!")
+                    else:
+                        print("Koden er feil!")
+                case _:
+                    break
+
     def _success(self):
         print(f"Du har blitt logget inn som {self._username}!")
         while True:
@@ -109,6 +171,7 @@ Du kan:
 - (l)ogg ut
 - (s)topp programmet
 - (a)lle brukere
+- (t)ofaktor autentisering
             """)
             action = self._input("Hva vil du gjøre? ")
             print()
@@ -122,6 +185,8 @@ Du kan:
                     break
                 case "a":
                     self._list_users()
+                case "t":
+                    self._config_2fa()
                 case _:
                     print("Det er ikke et gyldig valg!")
                     continue
@@ -133,4 +198,4 @@ if __name__ == "__main__":
         login.main()
     except (KeyboardInterrupt, EOFError):
         print()
-        print("Hade")
+        print(":(")
