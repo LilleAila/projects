eb6e6b8896c32d7619ea9a0594e644f19218927a
LilleAila <olai.solsvik@gmail.com>
2024-09-07 19:37:27 +0200

    skrive oppgaven i 03

--- it2/03-bruker-innlogging/main.py
+++ it2/03-bruker-innlogging/main.py
@@ -1,3 +1,13 @@
+"""
+Oppgave:
+    - Grunnleggande oppgåve: Lag eit system som handterer innlogging. Ha eit fokus på korleis du kan sjekke kriterier på best måte (kva er "best"?). Brukaren må skrive inn brukarnavn og passord, og skal få tydelege feilmeldingar. NB: Særs forenkla når det gjeld lagring av brukarnavn og passord, kryptering mm. Denne oppgåva bør du absolutt diskutere og samanlikne med naboane, då det er mange moglege løysingar.
+    - Utvida, og meir avansert oppgåve: Tenk sikkerheit.
+
+Lisens: GNU General Public License v3.0
+
+Skrevet av Olai Solsvik
+"""
+
 from src.login import Login
 
 # Sjekke at programmet blir kjørt direkte. Hvis den blir importert som en module, skal den ikke kjøre koden.
