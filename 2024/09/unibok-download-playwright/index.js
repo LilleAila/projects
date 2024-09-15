a63082b4b99aad46b760fec4d7b4daf5586fee63
LilleAila <olai.solsvik@gmail.com>
2024-09-15 13:46:10 +0200

    mer unibok ting

--- 2024/09/unibok-download-playwright/index.js
+++ 2024/09/unibok-download-playwright/index.js
@@ -16,7 +16,7 @@ const main = async () => {
 
   // needs to be filled before button can be pressed
   await page.fill("input#org_selector_filter", " ");
-  await page.click(`li[org_id="vlfk.no"]`);
+  await page.click(`li[org_id="${process.env.FEIDE_ORG_NAME}"]`);
   await page.click("button#selectorg_button");
 
   await page.waitForSelector("input#username");
@@ -27,22 +27,3 @@ const main = async () => {
 };
 
 main();
-
-/*
-import puppeteer from "puppeteer-core";
-
-const browser = await puppeteer.launch({
-  executablePath:
-    "/nix/store/irznr30hbryms34qhgc2g7jnap962xif-google-chrome-127.0.6533.119/bin/google-chrome-stable",
-});
-const page = await browser.newPage();
-
-await page.goto("https://unibok.no/feidelogin");
-
-await page.setViewport({ width: 1080, height: 1080 });
-await page.locator("input#username").fill("e-olasol2");
-await page.locator("input#password").fill("mitt passord");
-// await page.locator(`.main .breathe-top button.button-primary`).click();
-
-// await browser.close();
-*/
