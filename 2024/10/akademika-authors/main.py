3cd39e4d837aa74c35e3364bc6504c39af61cc9d
LilleAila <olai.solsvik@gmail.com>
2024-10-06 16:03:58 +0200

    akademika author to calibre format

---
+++ 2024/10/akademika-authors/main.py
@@ -0,0 +1,23 @@
+import os
+
+names = os.popen("wl-paste").read().strip()
+
+names = names.split()
+full_names = []
+
+buf = []
+for name in names:
+    if "," in name:
+        if len(buf) > 0:
+            full_names.append(buf)
+            buf = []
+        buf.append(name[:-1])
+    else:
+        buf.append(name)
+full_names.append(buf)
+
+full_names = [i[1:] + [i[0]] for i in full_names]
+
+output = " & ".join([" ".join(i) for i in full_names])
+
+os.system(f"wl-copy '{output}'")
---
+++ 2024/10/akademika-authors/main.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+authors=$(wl-paste)
+
+echo $authors
