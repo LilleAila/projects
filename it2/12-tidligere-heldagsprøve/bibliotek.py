9c14d83bcaa055a57c7d5733ed6e512385120bd6
LilleAila <olai.solsvik@gmail.com>
2024-11-25 18:42:44 +0100

    more book stuff

--- it2/12-tidligere-heldagsprøve/bibliotek.py
+++ it2/12-tidligere-heldagsprøve/bibliotek.py
@@ -5,8 +5,11 @@ type ISBN = int
 type Genre = str
 GENRES: list[Genre] = ["Action", "Humor", "Science Fiction", "Mystery", "Nonfiction"]
 
+type Edition = str
+EDITIONS: list[Edition] = ["Physical", "Digital", "Audiobook"]
 
-class Author(str):
+
+class Author:
     __slots__ = "__names"
 
     def __init__(self, name: str) -> None:
@@ -45,7 +48,8 @@ class Book:
         "__authors",
         "__isbn",
         "__published",
-        "__genres",
+        "__edition",
+        "__genre",
         "__borrow_date",
         "__due_date",
         "__borrowed",
@@ -57,14 +61,16 @@ class Book:
         authors: list[Author],
         isbn: ISBN,
         published: datetime.date,
-        genres: list[Genre] = [],
+        genre: Genre | None = None,
+        edition: Edition = EDITIONS[0],
         borrow_date: None | datetime.date = None,
     ) -> None:
         self.__title = title.capitalize()
         self.__authors = authors
         self.__isbn = isbn
         self.__published = published
-        self.__genres = genres
+        self.__edition = edition
+        self.__genre = genre
         self.__borrow_date = borrow_date
         if borrow_date is not None:
             self.borrow(borrow_date)
@@ -90,8 +96,12 @@ class Book:
         return self.__published
 
     @property
-    def genres(self) -> list[Genre]:
-        return self.__genres
+    def genre(self) -> Genre | None:
+        return self.__genre
+
+    @property
+    def edition(self) -> Edition:
+        return self.__edition
 
     @property
     def borrow_date(self) -> datetime.date:
@@ -129,11 +139,11 @@ class Book:
         self.__due_date = None
 
     def __repr__(self) -> str:
-        authors = " & ".join(self.authors)
+        authors = " & ".join(map(str, self.authors))
         if self.borrowed:
-            return f"{self.title} - {authors} - Borrowed {str(self.borrow_date)}, Due {str(self.due_date)}"
+            return f"{self.edition}, {self.genre} - {self.title} - {authors} - Borrowed {str(self.borrow_date)}, Due {str(self.due_date)}"
         else:
-            return f"{self.title} - {authors} - Not borrowed"
+            return f"{self.edition}, {self.genre} - {self.title} - {authors} - Not borrowed"
 
 
 class Library:
@@ -146,13 +156,20 @@ class Library:
                 [Author("Douglas Adams")],
                 9780307417138,
                 datetime.date(1979, 10, 15),
-                genres=[GENRES[1], GENRES[2]],
+                GENRES[1],
+                EDITIONS[0],
             )
         ]
 
     @property
     def books(self) -> list[Book]:
         return self.__books
+
+    def add_book(self, book: Book) -> None:
+        self.__books.append(book)
+
+    def search(self, query: str) -> list[Book]:
+        return [book for book in self.books if query.lower() in book.title.lower()]
 
     def __repr__(self) -> str:
         num_books = len(self.books)
@@ -167,48 +184,99 @@ class TUI:
         self.__running = False
         self.__library = Library()
 
-    def __input(self, prompt: str) -> str:
+    def __input(self, prompt: str, predicate=lambda x: len(x) > 0) -> str:
         while True:
             result = input(prompt)
-            if len(result) > 0:
+            if predicate(result):
                 return result
+
+    def __input_int(self, prompt: str) -> int:
+        while True:
+            try:
+                return int(self.__input(prompt))
+            except ValueError:
+                pass
+
+    def __input_date(self, prompt):
+        while True:
+            result = input(prompt)
+            try:
+                return datetime.datetime.strptime(result, "%Y-%m-%d")
+            except ValueError:
+                print(f"Not a valid date: {result}")
+
+    def __input_authors(self) -> list[Author]:
+        authors = []
+        print("Input the names of the authors. To finish, press enter.")
+        while True:
+            result = input("Author's full name: ")
+            if len(result) > 0:
+                authors.append(Author(result))
+            else:
+                return authors
 
     def __choose_action(self, actions: dict[str, str]) -> str:
         actions_prompt = "\n".join(
             [f"{key}: {action}" for key, action in actions.items()]
         )
-        try:
+        print()
+        while True:
+            result = input(actions_prompt + "\nChoose an action: ")
             print()
-            while True:
-                result = self.__input(actions_prompt + "\nChoose an action: ")
-                print()
-                if result in actions:
-                    return result
-                else:
-                    print("Invalid answer! Try again.")
-        except (KeyboardInterrupt, EOFError):
-            self.__running = False
-            return ""
+            if result in actions:
+                return result
+            else:
+                print("Invalid answer! Try again.")
 
-    def __list_books(self) -> str:
+    def __list_books(self, books) -> None:
         books = map(str, self.__library.books)
-        return "\n".join(books)
+        print("\n".join(books))
+
+    def __add_book(self) -> None:
+        title = self.__input("Book title: ")
+        authors = self.__input_authors()
+        isbn = self.__input_int("Book ISBN: ")
+        published = self.__input_date("Publishing date in format YYYY-MM-DD: ")
+        genres = ", ".join(GENRES)
+        genre = self.__input(
+            f"{genres}\nChoose one of the above genres: ",
+            predicate=lambda x: x in GENRES,
+        )
+        editions = ", ".join(EDITIONS)
+        edition = self.__input(
+            f"{editions}\nChoose one of the above editions: ",
+            predicate=lambda x: x in EDITIONS,
+        )
+        book = Book(title, authors, isbn, published, genre, edition)
+        self.__library.add_book(book)
+
+    def __search_book(self) -> None:
+        query = self.__input("Search query:")
+        print("Books matching your query:")
+        result = self.__library.search(query)
+        self.__list_books(result)
 
     def start(self) -> None:
         self.__running = True
         print("Welcome to the library!")
-        while self.__running:
-            action = self.__choose_action(
-                {
-                    "l": "List books",
-                    "a": "Add book",
-                    "s": "Search for book",
-                    "b": "Borrow book",
-                }
-            )
-            match action:
-                case "l":
-                    print(self.__list_books())
+        try:
+            while self.__running:
+                action = self.__choose_action(
+                    {
+                        "l": "List books",
+                        "a": "Add book",
+                        "s": "Search for book",
+                    }
+                )
+                match action:
+                    case "l":
+                        self.__list_books(self.__library.books)
+                    case "a":
+                        self.__add_book()
+                    case "s":
+                        self.__search_book()
+        except (KeyboardInterrupt, EOFError):
+            self.__running = False
 
 
 if __name__ == "__main__":
