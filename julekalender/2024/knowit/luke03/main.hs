54d32a4546738e214cc9a2c7b20d77da49f470a3
LilleAila <olai.solsvik@gmail.com>
2024-12-03 18:32:09 +0100

    more luke 3

--- julekalender/2024/knowit/luke03/main.hs
+++ julekalender/2024/knowit/luke03/main.hs
@@ -30,7 +30,7 @@ refillFood :: Food -> Refill -> Int -> Food
 refillFood [] _ _ = []
 refillFood food@((name, amount, n, s) : xs) refill seconds
   | name == "gulrøtter" = if seconds >= 30 then (name, amount + refillAmount, n + 1, s + 1) : refillFood xs refill seconds else (name, amount, n, s + 1) : refillFood xs refill seconds
-  | name == "reinsdyrkjøtt" = if amount <= 0 && s >= 50 && n < 5 then (name, amount + refillAmount, n + 1, 0) : refillFood xs refill seconds else (name, amount, n, s + 1) : refillFood xs refill seconds
+  | name == "reinsdyrkjøtt" = if amount <= 0 && s > 50 && n < 5 then (name, amount + refillAmount, n + 1, 0) : refillFood xs refill seconds else (name, amount, n, s + 1) : refillFood xs refill seconds
   | name == "julekringle" = (name, amount, n, s + 1) : refillFood xs refill seconds
   | otherwise = (name, amount + refillAmount, n + 1, s + 1) : refillFood xs refill seconds
   where
@@ -42,9 +42,13 @@ eatTwo' :: [Int] -> Int -> Food -> Food
 eatTwo' _ _ [] = []
 eatTwo' [] _ x = x
 eatTwo' g@(gram : grams) eaten x@((name, amount, n, s) : xs)
-  | eaten >= 2 = x
-  | amount > 0 = (name, max 0 (amount - gram), n, s) : eatTwo' grams (eaten + 1) xs
-  | otherwise = (name, amount, n, s) : eatTwo' g eaten xs
+  | eaten >= 2 = (name, amount, n, s + 1) : eatTwo' g eaten xs
+  | name == "reinsdyrkjøtt" = if amount >= 2 && eaten == 0 then (name, max 0 (amount - 2), n, s) : eatTwo' grams (eaten + 2) xs else next
+  | name == "julekringle" = if amount >= 1 && eaten == 0 then (name, max 0 (amount - 1), n, s) : eatTwo' grams (eaten + 2) xs else next
+  | amount >= gram = (name, max 0 (amount - gram), n, s) : eatTwo' grams (eaten + 1) xs
+  | otherwise = next
+  where
+    next = (name, amount, n, s) : eatTwo' g eaten xs
 
 eatTwo :: Food -> Food
 eatTwo = eatTwo' [5, 3] 0
@@ -52,12 +56,16 @@ eatTwo = eatTwo' [5, 3] 0
 eatAndRefill :: Food -> Refill -> Int -> Food
 eatAndRefill food = refillFood (eatTwo food)
 
+eatAndRefill' food = eatAndRefill food foodRefill 0
+
+refillAndEat food refill seconds = eatTwo (refillFood food refill seconds)
+
 solve :: Food -> Refill -> Int -> Int
 solve food refill seconds
   | newFood `getFood` "julekringle" <= 99 = seconds + 1
   | otherwise = solve newFood refill (seconds + 1)
   where
-    newFood = eatAndRefill food refill seconds
+    newFood = refillAndEat food refill seconds
 
 main :: IO ()
 main = do
@@ -65,9 +73,14 @@ main = do
   -- print food
   -- print test
   -- print $ refillFood food foodRefill 0
-  -- let answer = solve food foodRefill 0
-  -- print answer
   print food
-  print $ eatAndRefill food foodRefill 0
-  print $ eatAndRefill (eatAndRefill food foodRefill 0) foodRefill 1
-  print $ eatAndRefill (eatAndRefill (eatAndRefill food foodRefill 0) foodRefill 1) foodRefill 2
+  -- print $ eatAndRefill food foodRefill 0
+  -- print $ eatAndRefill (eatAndRefill food foodRefill 0) foodRefill 1
+  -- print $ eatAndRefill (eatAndRefill (eatAndRefill food foodRefill 0) foodRefill 1) foodRefill 2
+  print $ eatAndRefill' food
+  print $ iterate eatAndRefill' food !! 22
+  print $ iterate eatAndRefill' food !! 23
+  print $ iterate eatAndRefill' food !! 24
+  -- print $ take 70 $ iterate eatAndRefill' food
+  let answer = solve food foodRefill 0
+  print answer
---
+++ julekalender/2024/knowit/luke03/main2.hs
@@ -0,0 +1,80 @@
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+
+-- Name, Amount, Number eaten, Seconds since eaten / seconds since start depending on name
+type Food = [(String, Int, Int, Int)]
+
+type Refill = Map.Map String [Int]
+
+foodTypes = ["ris", "erter", "gulrøtter", "reinsdyrkjøtt", "julekringle"]
+
+food :: Food
+food = map (,100,0,0) foodTypes
+
+getAmount :: Food -> String -> Int
+getAmount [] _ = 0
+getAmount ((name, amount, _, _) : food) target
+  | name == target = amount
+  | otherwise = getAmount food target
+
+foodRefill :: Refill
+foodRefill =
+  Map.fromList
+    [ ("ris", [0, 0, 1, 0, 0, 2]),
+      ("erter", [0, 3, 0, 0]),
+      ("gulrøtter", [0, 1, 0, 0, 0, 8]),
+      ("reinsdyrkjøtt", [100, 80, 40, 20, 10]),
+      ("julekringle", [0])
+    ]
+
+getRefill :: String -> [Int]
+getRefill name = fromMaybe [0] (Map.lookup name foodRefill)
+
+-- Add food
+refillFood :: Food -> Food
+refillFood [] = []
+refillFood x@((name, amount, n, s) : xs)
+  | name == "gulrøtter" = if s > 30 then (name, amount + nextRefill, n + 1, s + 1) : refillFood xs else noChange : refillFood xs
+  | name == "reinsdyrkjøtt" = if amount <= 0 then if s > 50 then (name, amount + if n < length refill then nextRefill else 0, n + 1, 0) : next else noChange : next else (name, amount, n, 0) : next
+  | name == "julekringle" = noChange : next
+  | otherwise = (name, amount + nextRefill, n + 1, s + 1) : next
+  where
+    refill = getRefill name
+    nextRefill = refill !! (n `mod` length refill)
+    noChange = (name, amount, n, s + 1)
+    next = refillFood xs
+
+-- Eat food
+eatFood' :: Int -> Food -> Food
+eatFood' _ [] = []
+eatFood' eaten x@((name, amount, n, s) : xs)
+  | eaten >= 2 = x
+  | name == "reinsdyrkjøtt" = if eaten == 0 && amount >= 2 then eat 2 : eatFood' (eaten + 2) xs else next
+  | name == "julekringle" = if eaten == 0 && amount >= 1 then eat 1 : eatFood' (eaten + 2) xs else next
+  | amount >= grams = eat grams : eatFood' (eaten + 1) xs
+  | otherwise = next
+  where
+    grams = if eaten == 0 then 5 else 3
+    next = (name, amount, n, s) : eatFood' eaten xs
+    eat x = (name, amount - x, n, s)
+
+eatFood :: Food -> Food
+eatFood = eatFood' 0
+
+refillAndEat :: Food -> Food
+refillAndEat = refillFood . eatFood
+
+solve :: Food -> Int
+solve food
+  | newFood `getAmount` "julekringle" <= 99 = 1
+  | otherwise = solve newFood + 1
+  where
+    newFood = refillAndEat food
+
+main :: IO ()
+main = do
+  -- print $ take 70 $ iterate refillAndEat food
+  print $ solve food
+  print $ refillFood [("ris", 0, 23, 23)]
+  print $ refillFood [("ris", 2, 24, 24)]
+  print $ refillFood [("ris", 2, 25, 25)]
