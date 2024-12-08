3c292cf374005ebb033d1a3619334db4ec30d12d
LilleAila <olai.solsvik@gmail.com>
2024-12-08 12:06:49 +0100

    knowit: luke 8

---
+++ julekalender/2024/knowit/luke08/main.hs
@@ -0,0 +1,34 @@
+import Data.Char (digitToInt)
+import Data.List (transpose)
+
+primes :: [Int]
+primes = 2 : 3 : sieve (tail primes) [5, 7 ..]
+  where
+    sieve (p : ps) xs = h ++ sieve ps [x | x <- t, x `rem` p /= 0]
+      where
+        (h, ~(_ : t)) = span (< p * p) xs
+
+isPrime :: Int -> Bool
+isPrime n = n `elem` takeWhile (<= n) primes
+
+multiplyPowers :: Int -> [[Int]] -> [[Int]]
+multiplyPowers n = zipWith (map . (*)) powers
+  where
+    powers = map (n ^) [0 ..]
+
+primtalv :: [Int]
+primtalv = map primtalv' [1 ..]
+  where
+    primtalv' :: Int -> Int
+    primtalv' n = result
+      where
+        primeNums = take n primes
+        primeDigits = (multiplyPowers 10 . map (map digitToInt) . transpose . map show) primeNums
+        result = (sum . map sum) primeDigits
+
+perfektePrimtalv :: [Int]
+perfektePrimtalv = filter isPrime primtalv
+
+main :: IO ()
+main = do
+  print $ (length . takeWhile (< 10_000_000)) perfektePrimtalv
