cdecf7414b8c799574c1d4c73e4b91b29e594039
LilleAila <olai.solsvik@gmail.com>
2025-01-20 18:06:17 +0100

    simplify api call

--- 2025/01/openstreetmap-api/main.py
+++ 2025/01/openstreetmap-api/main.py
@@ -22,37 +22,23 @@ query = f"""
   way["building"]({bbox[0]}, {bbox[1]}, {bbox[2]}, {bbox[3]});
   way["highway"]({bbox[0]}, {bbox[1]}, {bbox[2]}, {bbox[3]});
 );
-out body;
+out geom;
 """
 
 response = overpass.query(query)
 assert response is not None, "No location found"
 features = response.toJSON()["elements"]
 
-node_coordinates = {}
-node_ids = {node_id for feature in features for node_id in feature["nodes"]}
-node_query = f"""
-node(id:{",".join(map(str, node_ids))});
-out;
-"""
-node_response = overpass.query(node_query)
-assert node_response is not None, "no nodes found?"
-for node in node_response.toJSON()["elements"]:
-    node_coordinates[node["id"]] = (node["lon"], node["lat"])
 
 building_polygons = []
 road_lines = []
 for feature in features:
-    if "nodes" in feature and feature["type"] == "way":
-        try:
-            points = [node_coordinates[node_id] for node_id in feature["nodes"]]
-            if feature["type"] == "way":
-                if "building" in feature.get("tags", {}):
-                    building_polygons.append(Polygon(points))
-                elif "highway" in feature.get("tags", {}):
-                    road_lines.append(LineString(points))
-        except KeyError:
-            continue
+    if "geometry" in feature:
+        points = [(node["lon"], node["lat"]) for node in feature["geometry"]]
+        if "building" in feature.get("tags", {}):
+            building_polygons.append(Polygon(points))
+        elif "highway" in feature.get("tags", {}):
+            road_lines.append(LineString(points))
 
 gdf_buildings = gpd.GeoDataFrame(geometry=building_polygons)
 gdf_roads = gpd.GeoDataFrame(geometry=road_lines)
@@ -60,9 +46,7 @@ gdf_roads = gpd.GeoDataFrame(geometry=road_lines)
 fig, ax = plt.subplots(figsize=(10, 10))
 gdf_buildings.plot(ax=ax, color="black", edgecolor="white", label="Buildings")
 gdf_roads.plot(ax=ax, color="blue", linewidth=0.5, label="Roads")
-ax.set_title("Buildings and Roads Map")
+ax.set_title("Bærgen")
 ax.set_xlabel("Longitude")
 ax.set_ylabel("Latitude")
-plt.grid(True)
-plt.legend()
 plt.show()
