c3f608c0231b872924542ea58c808938266a4fbe
LilleAila <olai.solsvik@gmail.com>
2025-02-06 12:55:25 +0100

    cstimer plotting lol

--- it2/20-datasett-prosjekt/main.py
+++ it2/20-datasett-prosjekt/main.py
@@ -1,8 +1,42 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import re
 
 df = pd.read_csv("cstimer-3x3.csv", delimiter=";")
 df["Date"] = pd.to_datetime(df["Date"])
 
+
+def parse_time(time):
+    pattern = r"(?:(\d)+:)?(\d+)\.(\d+)"
+    match = re.match(pattern, time)
+    if match:
+        mins = int(match.group(1)) if match.group(1) else 0
+        seconds = int(match.group(2))
+        hundredths = int(match.group(3))
+        milliseconds = hundredths * 10 + seconds * 1000 + mins * 60000
+        return pd.Timedelta(milliseconds, "ms")
+    else:
+        return np.nan
+
+
+df["Time"] = df["Time"].apply(parse_time)
+df = df.dropna(subset=["Time"])
+df["Seconds"] = df["Time"].dt.total_seconds()
 print(df)
+
+fig, ax = plt.subplots()
+
+df.plot(
+    ax=ax,
+    x="No.",
+    y=["Seconds"],
+    title="Solve time in seconds",
+    xlabel="Solve number",
+    ylabel="Time in seconds",
+)
+
+trend_line = np.polyval(np.polyfit(df["No."], df["Seconds"], 1), df["No."])
+ax.plot(df["No."], trend_line, linestyle="--", label="Trend line")
+ax.legend()
+plt.show()
