d643ed575b3eb003b39ef7a881c10d2d30f7f3c2
LilleAila <olai.solsvik@gmail.com>
2025-02-08 19:38:32 +0100

    more koreader

--- it2/20-datasett-prosjekt/05-koreader.py
+++ it2/20-datasett-prosjekt/05-koreader.py
@@ -17,12 +17,42 @@ import pandas as pd
 
 books = pd.read_csv("05-koreader-combined-books.csv")
 pages = pd.read_csv("05-koreader-combined-pages.csv")
+pages["date"] = pd.to_datetime(pages["start_time"], unit="s")
 
+# Visualize minutes per page, for each book.
 books["minutes_per_page"] = books["total_read_time"] / books["total_read_pages"] / 60
 books = books.sort_values("minutes_per_page")
 
-print(books)
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
-books.plot(kind="barh", x="title", y="minutes_per_page", ylabel="")
-plt.subplots_adjust(left=0.4)
+books.plot(ax=ax1, kind="barh", x="title", y="minutes_per_page", ylabel="")
+# plt.subplots_adjust(left=0.4)
+# ax1.set_position([0.35, 0.1, 0.15, 0.8])
+pos = ax1.get_position()
+ax1.set_position([pos.x0 + 0.15, pos.y0, pos.width - 0.15, pos.height])
+ax1.set_yticklabels(
+    ax1.get_yticklabels(), rotation=15, ha="right", rotation_mode="anchor"
+)
+ax1.set_title("Average time spent per page by book")
+
+# Visualize time spent on each page over time
+# pages_fr = pages[pages["id_book"] == 7]
+pages_by_book = pages.groupby("id_book")
+pages_fr = pages_by_book.get_group(6).sort_values("page")  # thhgttg
+pages_fr.plot(ax=ax2, kind="line", x="page", y="duration")
+pages_fr["rolling_avg"] = pages_fr["duration"].rolling(window=20).mean()
+pages_fr.plot(
+    ax=ax2, kind="line", x="page", y="rolling_avg", color="orange", linewidth=2
+)
+ax2.set_title("Time spent per page: The hitchhiker's guide to the galaxy")
+
+# Most productive days
+pages["weekday"] = pages["date"].dt.day_name()
+weekdays = pages["weekday"].value_counts(sort=False)
+weekdays.plot(ax=ax3, kind="bar")
+ax3.set_xticklabels(
+    ax3.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor"
+)
+ax3.set_title("Pages read by day")
+
 plt.show()
