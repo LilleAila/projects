55f6708b4f6027201fafdd07b8675821a99c22d3
LilleAila <olai.solsvik@gmail.com>
2025-02-09 11:54:35 +0100

    anki review efficiency

--- it2/20-datasett-prosjekt/04-anki-reviews.py
+++ it2/20-datasett-prosjekt/04-anki-reviews.py
@@ -6,15 +6,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("04-anki-reviews.csv")
-df["date"] = pd.to_datetime(df["id"], unit="ms")
+df["date"] = (
+    pd.to_datetime(df["id"], unit="ms")
+    .dt.tz_localize("UTC")
+    .dt.tz_convert("Europe/Oslo")
+)
+df["hour"] = df["date"].dt.hour
 df["time"] = pd.to_timedelta(df["time"], unit="ms")  # type: ignore # lsp complains again, idk why
 df["ivl"] = df["ivl"].apply(
     lambda x: pd.to_timedelta(x, unit="D") if x > 0 else pd.to_timedelta(-x, unit="s")
 )
+df["lastIvl"] = df["lastIvl"].apply(
+    lambda x: pd.to_timedelta(x, unit="D") if x > 0 else pd.to_timedelta(-x, unit="s")
+)
+df["efficiency"] = df["ease"] / df["time"].dt.total_seconds()
 
 cards = df.drop_duplicates(subset="cid", keep="last")
 
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8)) = plt.subplots(2, 4)
 fig.suptitle("Anki review statistics")
 
 df["date"].hist(ax=ax1, bins=20, grid=False)
@@ -45,7 +54,7 @@ ax5.set_xlabel("Interval in days")
 ax5.set_ylabel("Cards")
 
 df["date"].dt.weekday.value_counts().sort_index().plot(ax=ax6, kind="bar", xlabel="")
-ax6.set_title("Total reviews by weekday")
+ax6.set_title("Reviews by weekday")
 ax6.set_xticklabels(
     ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     rotation=45,
@@ -53,4 +62,15 @@ ax6.set_xticklabels(
 )
 ax6.set_ylabel("Reviews")
 
+df["date"].dt.hour.value_counts().sort_index().plot(ax=ax7, kind="bar", xlabel="")
+ax7.set_title("Reviews by time of day")
+ax7.set_xlabel("Hour")
+ax7.set_ylabel("Reviews")
+
+df.groupby("hour")["efficiency"].mean().plot(ax=ax8, kind="bar")
+ax8.set_title("Review efficiency time of day")
+ax8.set_xlabel("")
+ax8.set_ylabel("Average efficiency (ease / time)")
+
+fig.subplots_adjust(wspace=0.3, hspace=0.25)
 plt.show()
