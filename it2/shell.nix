c12c22871fe084b3370ca376a79fb14cf4b77f51
LilleAila <olai.solsvik@gmail.com>
2025-05-12 10:32:15 +0200

    it2: pip

--- it2/shell.nix
+++ it2/shell.nix
@@ -18,6 +18,8 @@ with import <nixpkgs> {}; mkShell {
       pytest
       pytest-html
 
+      pip
+
       # Run with python3 -m jupyterlab. Don't know which of the three are actually needed.
       jupyter
       jupyterlab
