a71586494445a3435ce6ef7a16e13d60a7117570
LilleAila <olai.solsvik@gmail.com>
2025-12-09 10:05:06 +0100

    knowit 09

---
+++ julekalender/2025/knowit/09/main.hs
@@ -0,0 +1,16 @@
+import Data.Char (isDigit)
+
+isConsonant :: Char -> Bool
+isConsonant = (`elem` "bcdfghjklmnpqrstvwxz")
+
+bean xs i
+  | isDigit x = False
+  | isConsonant x = (i + 2 < length xs && i - 2 >= 0) && (isDigit (xs !! (i + 2)) && isDigit (xs !! (i - 2)))
+  | otherwise = True
+  where
+    x = xs !! i
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  print $ map (input !!) (filter (bean input) [0 .. length input - 1])
